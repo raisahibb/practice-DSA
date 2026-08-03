17460e489ec1c5fd31d60f9f409bd3d297f55fb4
Rai Sahib <sidhuc888@gmail.com>
2026-08-03 11:27:59 +0530

    Time: 11 ms (90.58%), Space: 136.2 MB (76.04%) - LeetHub

--- 1522-stone-game-iii/1522-stone-game-iii.cpp
+++ 1522-stone-game-iii/1522-stone-game-iii.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    static constexpr int MIN = -50000001;
-    static inline string s[] = {"Bob", "Tie", "Alice"};
-
-    string stoneGameIII(vector<int>& A) {
-        int n = A.size();
-        vector<int> dp(n, MIN);
-
-        auto maxDiff = [&](this auto&& maxDiff, int i) -> int {
-            if (i == n) return 0;
-
-            int& res = dp[i];
-            if (res != MIN) return res;
-
-            int a = MIN, b = MIN, c = MIN;
-
-            if (i < n)
-                a = A[i] - maxDiff(i + 1);
-            if (i + 1 < n)
-                b = A[i] + A[i + 1] - maxDiff(i + 2);
-            if (i + 2 < n)
-                c = A[i] + A[i + 1] + A[i + 2] - maxDiff(i + 3);
-
-            res = max(a, max(b, c));
-            return res;
-        };
-
-        int d = maxDiff(0);
-        return s[(d > 0) - (d < 0) + 1];
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n=stoneValue.size();
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            int sum=0;
+            dp[i]=INT_MIN;
+            for(int k=0;k<3&&i+k<n;k++){
+                sum+=stoneValue[i+k];
+                dp[i]=max(dp[i],sum-dp[i+k+1]);
+            }
+        }
+        if(dp[0]>0) return "Alice";
+        if(dp[0]<0) return "Bob";
+        return "Tie";
     }
 };
